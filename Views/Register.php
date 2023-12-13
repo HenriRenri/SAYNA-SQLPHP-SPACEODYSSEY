0f942979eab40e6e677355177a487de9b22bae8f
HenriRenri <115990655+HenriRenri@users.noreply.github.com>
2023-12-13 22:47:49 +0300

    Ajouter des nouvelle contenue sur le projet

--- Views/Register.php
+++ Views/Register.php
@@ -1,22 +1,20 @@
 <?php 
 		##  ****************** Appelle sur l'entête de la page  **********************  ##
     
-    Initialisation::loadView('templates/header') ;
+    Initialisation::loadView('templates/header');
   
 ?>
 
     <div class="row mt-5 mb-5">
         <div class="col-md-4"></div>
         <div class="col-md-4">
-        <div class="login-box">
-             <!-- /.login-logo -->
+          <div class="login-box">
             <div class="card card-outline card-primary">
                <div class="card-header text-center">
                  <a href="../../index2.html" class="h1"><b>Stellar</b>Tech</a>
                </div>
                <div class="card-body">
-                 <p class="login-box-msg">Sign in to start your session</p>
-
+                 <p class="login-box-msg">Se conneté(e)</p>
                  <form action="../../index3.html" method="post">
                    <div class="input-group mb-3">
                      <input type="email" class="form-control" placeholder="Email">
@@ -44,7 +42,7 @@
                  <div class="row">
                     <div class="col-md-9">
                         <p class="mb-0">
-                            <a href="register.html" class="text-center">Register a new membership</a>
+                            <a href="#" class="text-center" data-toggle="modal" data-target="#modal-info">Crée une nouvelle compt</a>
                         </p>
                     </div>
                     <div class="col-md-3">
@@ -54,13 +52,85 @@
                     </div>
                  </div>
                </div>
-               <!-- /.card-body -->
-             </div>
-             <!-- /.card -->
             </div>
+          </div>
         </div>
         <div class="col-md-4"></div>
     </div>
+
+    <div class="modal fade" id="modal-info">
+      <div class="modal-dialog">
+        <div class="modal-content bg-info">
+          <div class="modal-header">
+            <h4 class="modal-title">Crée une nouvelle compt</h4>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+            <div class=" ">
+              <div class="card-header text-center">
+                <a href="#" class="h1"><b>Stellar</b>Tech</a>
+              </div>
+              <div class="card-body">
+                <form action="../../index.html" method="post">
+                  <div class="input-group mb-3">
+                    <input type="text" class="form-control" placeholder="Full name">
+                    <div class="input-group-append">
+                      <div class="input-group-text">
+                        <span class="fas fa-user"></span>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="input-group mb-3">
+                    <input type="email" class="form-control" placeholder="Email">
+                    <div class="input-group-append">
+                      <div class="input-group-text">
+                        <span class="fas fa-envelope"></span>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="input-group mb-3">
+                    <input type="password" class="form-control" placeholder="Password">
+                    <div class="input-group-append">
+                      <div class="input-group-text">
+                        <span class="fas fa-lock"></span>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="input-group mb-3">
+                    <input type="password" class="form-control" placeholder="Retype password">
+                    <div class="input-group-append">
+                      <div class="input-group-text">
+                        <span class="fas fa-lock"></span>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="col-8"></div>
+                    <!-- /.col -->
+                    <div class="col-4">
+                      <button type="submit" class="btn btn-outline-light">Enregistrer</button>
+                    </div>
+                    <!-- /.col -->
+                  </div>
+                </form>
+              </div>
+              <!-- /.form-box -->
+            </div><!-- /.card -->
+          </div>
+          <div class="modal-footer justify-content-between">
+            <button type="button" class="btn btn-outline-light" data-dismiss="modal">Déjat un membre</button>
+            <small class="syana">
+              <a href="https://sayna.space/" style="color: white;">by-@Sayna</a>
+            </small>
+          </div>
+        </div>
+        <!-- /.modal-content -->
+      </div>
+      <!-- /.modal-dialog -->
+    </div>
+      <!-- /.modal -->
 <?php 
 		##  ****************** Require le footer de la page  **********************  ##
 
--- Views/templates/header.php
+++ Views/templates/header.php
@@ -18,25 +18,25 @@
   <nav class="main-header navbar navbar-expand-md navbar-dark layout-navbar-fixed">
     <div class="container">
       <a href="#" class="navbar-brand">
-        <img src="Publics/image/logo.png" alt="Sayna Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+        <img src="Publics/Image/logo.png" alt="Sayna Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
         <span class="brand-text font-weight-light">Stellar Tech</span>
       </a>
 
       <ul class="order-1 order-md-3 navbar-nav navbar-no-expand ml-auto">
         <li class="nav-item">
-          <a href="index3.html" class="nav-link">Home</a>
+          <a href="Views/Home.php" class="nav-link">Home</a>
         </li>
         <li class="nav-item">
-          <a href="index3.html" class="nav-link">Planèt</a>
+          <a href="Views/Planet.php" class="nav-link">Planèt</a>
         </li>
         <li class="nav-item">
-          <a href="index3.html" class="nav-link">Astronaut</a>
+          <a href="Views/Astronaut" class="nav-link">Astronaut</a>
         </li>
         <li class="nav-item">
-          <a href="index3.html" class="nav-link">Géstion mission</a>
+          <a href="Views/Mission.php" class="nav-link">Géstion mission</a>
         </li>
         <li class="nav-item">
-          <a href="index3.html" class="nav-link">Espace</a>
+          <a href="Views/Espace" class="nav-link">Espace</a>
         </li>
       </ul>
     </div>
